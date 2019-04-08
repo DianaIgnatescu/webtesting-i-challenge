1fb35e393d9bf74e1725ffa4092b2791b52dab3a
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-08 12:39:40 +0100

    MVP: Write test that checks whether durability restored to 100 on repair

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,21 @@
-const enhancer = require('./enhancer.js');
+const { repair } = require('./enhancer.js');
 // test away!
+
+describe('enhancers', () => {
+  describe('repair function', () => {
+    it('should restore the durability of an item to 100', () => {
+      const originalItem = {
+        name: 'sword',
+        durability: 43,
+        enhancement: 15,
+      };
+      const expected = {
+        name: 'sword',
+        durability: 100,
+        enhancement: 15,
+      };
+      const actual = repair(originalItem);
+      expect(actual).toEqual(expected);
+    });
+  });
+});
